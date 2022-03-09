e50f7088b2ab409e881a1d90482fd7ed39156815
Stephanie Mathis <stephaniedmathis@gmail.com>
2022-03-08 21:21:53 -0500

    add new folders and files to repo

---
+++ index.js
@@ -0,0 +1,12 @@
+function addManager() {
+    // ask the managers info
+    // save it
+    // call mainMenu() function
+  }
+
+  function mainMenu() {
+    // add an engineer, intern or build the team HTML page?
+    // if you chose add an engineer then we need to call addEngineer()
+    // if you chose add an intern then we need to call addIntern()
+    // else buildTeam()
+  }
